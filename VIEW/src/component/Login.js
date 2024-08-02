fd9fe9b7649415542c69e8644e0e3d4c58617d54
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-08-02 14:17:32 +0300

    first commit nigga

---
+++ VIEW/src/component/Login.js
@@ -0,0 +1,18 @@
+import React ,{Component} from "react";
+
+class Login extends Component{
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+                <h1>Hello</h1>
+        )
+    }
+}
+
+export default Login;
