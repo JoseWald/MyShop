37d94fee1e359dc6c633920959584c06445e4a50
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-08-02 15:38:29 +0300

    ajout de lien pour l\'image et tentation d\'import de reactRouter

--- VIEW/src/component/Login.js
+++ VIEW/src/component/Login.js
@@ -1,4 +1,6 @@
 import React ,{Component} from "react";
+import {Link} from 'react-router-dom';
+import userPNG from "../IMG/user.png"
 
 class Login extends Component{
     constructor(){
@@ -15,7 +17,7 @@ class Login extends Component{
                     <div className="p-4   my-3 mx-auto col-md-6 ">
                       <h1 className="text-primary text-center">Sign in to your account</h1>
                       <div className="text-center">
-                       <img src="../img/user.png" alt="" className="mx-auto w-75 my-3"/>
+                       <img src={userPNG} alt="userPNG" className="mx-auto w-75 my-3"/>
                       </div>
                     </div>
                     <div className="form-signup  p-4  my-5 mx-auto col-md-6 ">
@@ -30,7 +32,6 @@ class Login extends Component{
 
                         </div>
                         <button type="submit" className="btn btn-primary w-100 my-3 p-2 shadow">Login</button>
-                        <p className="text-center text-dark"> Forgot password? </p>
                         <a href="Signup.html"><button type="button" className="btn y text-primary w-100 mt-4 p-2 shadow" id="signup">Signup</button></a>
                       </form>
                     </div>
