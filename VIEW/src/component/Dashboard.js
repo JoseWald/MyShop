3fa935c040726ead919b6a53a1c0c2abcbdc8191
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-08-03 08:50:47 +0300

    creation de dashboard.js

---
+++ VIEW/src/component/Dashboard.js
@@ -0,0 +1,21 @@
+import React,{Component} from "react";
+import { Link } from "react-router-dom";
+
+class Dashboard extends Component{
+        constructor(){
+            super();
+            this.state={
+
+            };
+        }
+
+        render(){
+            return(
+                    <div>
+                        Hello
+                    </div>
+            );
+        }
+}
+
+export default Dashboard;
